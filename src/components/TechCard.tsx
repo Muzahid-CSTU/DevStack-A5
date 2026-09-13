c74f7351bf2502a7012588ca04e60a61ac780470
Muzahid-CSTU <kmujahidulislamm@gmail.com>
2026-09-13 23:40:11 +0600

    Stack Cards Added

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import type { ItechList } from './Types/techListType';
 
-const TechCard = ({TechList}) => {
+interface TechCardProps {
+    TechList: ItechList[];
+}
+
+const TechCard = ({ TechList }: TechCardProps) => {
     return (
-        <div className='grid grid-cols-3'>
-        {
-            TechList.map((Tech : ItechList) =>{
-                return <div>
-                    {Tech.name}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {TechList.map((Tech: ItechList) => (
+                <div key={Tech.name} className="border border-gray-200 rounded-2xl p-4 bg-white">
+                    <div className="flex justify-between items-start mb-4">
+                        <img src={Tech.icon} alt={Tech.name} className="h-9 w-9 object-contain"/>
+                        <div className="text-sm border rounded-full px-3 py-1">{Tech.badge}</div>
+                    </div>
+                    <h2 className="text-lg font-semibold mb-2">{Tech.name}</h2>
+                    <p className="text-sm text-gray-500 leading-5 min-h-[60px]">{Tech.description}</p>
+                    <div className="flex items-center justify-between text-xs text-gray-500 mt-4 mb-4">
+                        <div className="bg-gray-100 px-2 py-1 rounded">{Tech.category}</div>
+                        <div>{Tech.difficulty}</div>
+                        <div>⭐ {Tech.rating}</div>
+                    </div>
+
+                    <button className="btn w-full bg-black text-white">Add to Stack</button>
                 </div>
-            })
-        }
+            ))}
         </div>
     );
 };
--- src/components/TechList.tsx
+++ src/components/TechList.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { use } from 'react'
+import { use, useState } from 'react'
 import type { ItechList } from "./Types/techListType";
 import TechCard from '../components/TechCard.tsx'
+import Sideber from './Sideber.tsx';
 
 interface techListProps {
   techListPromise: Promise<ItechList[]>;
@@ -13,12 +14,11 @@ const TechList = ({techListPromise} : techListProps) => {
     return (
         <div className='container mx-auto max-w-6xl px-8'>
             <div className='grid grid-cols-10 gap-4 mt-10'>
-                <div className='col-span-8 border'>
-                    <h1>Cards</h1>
+                <div className='col-span-8'>
                     <TechCard TechList={TechList} />
                 </div>
                 <div className='col-span-2 border'>
-                    <h1>Sideber</h1>
+                    <Sideber />
                 </div>
             </div>
 
