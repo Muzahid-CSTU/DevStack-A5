95d8292f3f674a9ec27a63f0bc8c682e814151e1
Muzahid-CSTU <kmujahidulislamm@gmail.com>
2026-09-14 17:26:26 +0600

    responsiveness updated

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,13 +1,12 @@
-
 const MainLayout = () => {
     return (
-        <div className=" mt-10 md:mt-28 max-w-6xl mx-auto">
-            <div className="px-8">
-                <h1 className="text-base md:text-4xl font-extrabold">
+        <div className="mt-10 md:mt-28 max-w-6xl mx-auto">
+            <div className="px-8 text-center md:text-left">
+                <h1 className="text-2xl md:text-4xl font-extrabold">
                     <span className="text-[#0F172A]">Explore the </span>
                     <span className="text-brand-gradient">Technologies</span>
                 </h1>
-                <p className="text-base font-normal text-[#64748B]">
+                <p className="text-xs md:text-base font-normal text-[#64748B] mt-1">
                     Pick one technology per category to build your ideal stack.
                 </p>
             </div>
